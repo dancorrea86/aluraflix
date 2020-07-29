40e6d611101c4027c681521faf2fec36c8dc87ea
dancorrea86 <danielmacielcorrea@gmail.com>
2020-07-28 21:21:33 -0300

    funcionando

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './pages/Home/App';
 
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+// Desafio master blaster na descrição
+const Pagina404 = () => (<div>Página 404</div>)
 
 ReactDom.render(
     <BrowserRouter>
-        
-        <App></App>
+        <Switch>
+            <Route path="/" component={App} exact/>
+            <Route path="/cadastro/video" component={App} />
+            <Route component={Pagina404} />
+        </Switch>
     </BrowserRouter>,
     document.getElementById('root')
 );
