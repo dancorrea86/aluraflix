0d734c879cfb3369b2fa4da1f80b5a10a2e7e900
dancorrea86 <danielmacielcorrea@gmail.com>
2020-07-30 08:57:53 -0300

    input funcionando

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageDefault from '../../../components/PageDefault' 
 import { Link } from 'react-router-dom';
 
  
 
 function CadatroCategoria() {
+    const [nomeDaCategoria, setNomeDaCategoria] = useState('Filmes');
+
+    console.log('[nomedacategoria]', nomeDaCategoria)
+
+
     return (
         <>
             <PageDefault>
-                <h1>Cadastro categoria</h1>
+                <h1>Cadastro categoria: {nomeDaCategoria}</h1>
 
                 <form>
                     <label>
                         Nome da Categoria:
-                        <input type="text"/>
+                        <input type="text"
+                        value={nomeDaCategoria}
+                        onChange={function funcaoHandleQueErroPediu(infosDoEvento) {
+                            console.log('[infoDoEvento]', infosDoEvento.target.value)
+                            console.log('[nomedacategoria.target.value]', nomeDaCategoria)
+                            setNomeDaCategoria(infosDoEvento.target.value)
+                        }}/>
                     </label>
                     
                     <button>
